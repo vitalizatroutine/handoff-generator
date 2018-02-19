42502e99678ff20b77311b3a346988e24dd03945
Vitali Zatroutine <vitaliz@q4inc.com>
2018-02-19 18:21:58 -0500

    added simple input component and some temporary form css

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Footer from "./components/footer/footer";
+import Input from "./components/global/input/input";
+
+const options = {
+    one: '',
+    two: ''
+};
 
 class App extends Component {
     render() {
         return (
             <main className="page">
+                <form className={"form"}>
+                    <div className={"form_item"}>
+                        <Input label={"First Input"} value={options.one} placeholder={"goldcorp2017ir"} required={true} />
+                    </div>
+                    <div className={"form_item"}>
+                        <Input label={"Second Input"} value={options.two} placeholder={"ir.goldcorp.com"} required={true} />
+                    </div>
+                </form>
                 <Footer />
             </main>
         );
